87092baf250ffe3ca6303444c54642008643ead9
dannycv <dannychaves1403@gmail.com>
2025-07-03 21:25:47 -0600

    Una modificacion nose de que jaja
    ni idea que fue lo q modifique ahi jaja

--- src/Evaluacion/Evaluaciones.java
+++ src/Evaluacion/Evaluaciones.java
@@ -4,10 +4,51 @@
  */
 package Evaluacion;
 
+import Curso.Cursos;
+
 /**
  *
  * @author danny
  */
 public class Evaluaciones {
+    private Cursos Curso;
+    private double nota;
+    private String observaciones;
+
+    public Evaluaciones(Cursos Curso, double nota, String observaciones) {
+        this.Curso = Curso;
+        this.nota = nota;
+        this.observaciones = observaciones;
+    }
+
+    public boolean satisfactorio() {
+        return nota > 8.0;
+    }
+    
+    
+    public Cursos getCurso() {
+        return Curso;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setCurso(Cursos Curso) {
+        this.Curso = Curso;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+    
     
 }
